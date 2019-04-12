693b5a8f32d24e267e7f6c85cbcfd6a1e761fb9d
quick0101 <49132945+quick0101@users.noreply.github.com>
2019-04-11 19:53:05 -0600

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,15 +1,10 @@
 <?php
 
-    /*Datos de conexion a la base de datos*/
-    /*
+	/*Datos de conexion a la base de datos*/
 	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 	define('DB_USER', 'root');//Usuario de tu base de datos
 	define('DB_PASS', '');//Contraseña del usuario de la base de datos
-	define('DB_NAME', 'bd_quick');//Nombre de la base de datos*/
-    define('DB_HOST', 'b34khezh7mt88aftjhqx-mysql.services.clever-cloud.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
-	define('DB_USER', 'unfxvkrdpztcqy9u');//Usuario de tu base de datos
-	define('DB_PASS', 'iq7c4PlB3OwxJ7UDsAac');//Contraseña del usuario de la base de datos
-	define('DB_NAME', 'b34khezh7mt88aftjhqx');//Nombre de la base de datos
+	define('DB_NAME', 'bd_quick');//Nombre de la base de datos
 
 	$con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(!$con){
