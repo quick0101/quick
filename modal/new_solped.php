6652703774bdcf00d151d5a50ae8af904988ba9e
quick0101 <49132945+quick0101@users.noreply.github.com>
2019-04-11 19:55:55 -0600

    Add files via upload

---
+++ modal/new_solped.php
@@ -0,0 +1,84 @@
+<?php
+   
+    $clientes =mysqli_query($con, "select * from r_user");
+    $estados =mysqli_query($con, "select * from r_estados");
+    $formapedido =mysqli_query($con, "select * from r_formapedido");
+
+?>
+
+    <div> <!-- Modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg-add"><i class="fa fa-plus-circle"></i> Agregar Item</button>
+    </div>
+    <div class="modal fade bs-example-modal-lg-add" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Agregar nuevo pedido</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal form-label-left input_mask" method="post" id="add" name="add">
+                        <div id="result"></div>
+                     
+                       
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Descripción <span class="required">*</span>
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                              <textarea name="descripcion" class="form-control col-md-7 col-xs-12"  placeholder="Descripción"></textarea>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Metodo de pedido
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="formapedido_id" >
+                                    <option selected="" value="">-- Selecciona --</option>
+                                      <?php foreach($formapedido as $p):?>
+                                        <option value="<?php echo $p['id_FormaPedido']; ?>"><?php echo $p['Nombre']; ?></option>
+                                      <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Cliente
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="cliente_id" >
+                                    <option selected="" value="">-- Selecciona --</option>
+                                      <?php foreach($clientes as $p):?>
+                                        <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                      <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                       
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Estado
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="estado_id" >
+                                    <option selected="" value="">-- Selecciona --</option>
+                                  <?php foreach($estados as $p):?>
+                                    <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                  <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                              <button id="save_data" type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                        </div>    
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Modal -->
---
+++ modal/upd_solped.php
@@ -0,0 +1,115 @@
+<?php
+    $projects =mysqli_query($con, "select * from r_clasificaciones");
+    $priorities =mysqli_query($con, "select * from r_prioridadades");
+    $statuses =mysqli_query($con, "select * from r_estados");
+    $kinds =mysqli_query($con, "select * from r_tipos");
+    $categories =mysqli_query($con, "select * from r_categorias");
+
+?>
+    <!-- Modal -->
+    <div class="modal fade bs-example-modal-lg-udp" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel"> Editar Registro</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal form-label-left input_mask" method="post" id="upd" name="upd">
+                        <div id="result2"></div>
+
+                        <input type="hidden" name="mod_id" id="mod_id">
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Tipo
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="kind_id" required id="mod_kind_id">
+                                      <?php foreach($kinds as $p):?>
+                                        <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                      <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Titulo<span class="required">*</span></label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                              <input type="text" name="title" class="form-control" placeholder="Titulo" id="mod_title" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Descripción <span class="required">*</span>
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                              <textarea name="description" id="mod_description" class="form-control col-md-7 col-xs-12" required></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Cantidad<span class="required">*</span></label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                              <input type="text" name="Cantidad" class="form-control" placeholder="Cantidad" id="mod_Cantidad" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Unidad de Medida
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="project_id" required id="mod_project_id">
+                                    <option selected="" value="">-- Selecciona --</option>
+                                      <?php foreach($projects as $p):?>
+                                        <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                      <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Categoria
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="category_id" required id="mod_category_id">
+                                    <option selected="" value="">-- Selecciona --</option>
+                                      <?php foreach($categories as $p):?>
+                                        <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                      <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Disponibilidad
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select class="form-control" name="priority_id" required id="mod_priority_id">
+                                    <option selected="" value="">-- Selecciona --</option>
+                                  <?php foreach($priorities as $p):?>
+                                    <option value="<?php echo $p['id']; ?>"><?php echo $p['name']." - (".$p['ndias']." dias)"; ?></option>
+                                  <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Estado
+                            </label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <select  class="form-control" name="status_id" required id="mod_status_id">
+                                    <option selected="" value="">-- Selecciona --</option>
+                                  <?php foreach($statuses as $p):?>
+                                    <option value="<?php echo $p['id']; ?>"><?php echo $p['name']; ?></option>
+                                  <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                              <button id="upd_data" type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                        </div>
+                    </form>                
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Modal -->
